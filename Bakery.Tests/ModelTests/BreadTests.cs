faa1cdf28fc21b64b95614e7f84226f0d0647719
Tim Roth <timdroth@gmail.com>
2021-09-11 20:43:59 -0700

    continue populating project structure

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void 
+  }
+}
---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void 
+  }
+}
