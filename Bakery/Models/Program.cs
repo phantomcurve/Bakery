4efd07b595b552c3fd064a59a9bb22a8a6eeea1b
Tim Roth <timdroth@gmail.com>
2021-09-13 20:45:17 -0700

    add functioning conditional comments for multi-buy prommotions

--- Bakery/Models/Program.cs
+++ Bakery/Models/Program.cs
@@ -9,8 +9,28 @@ namespace Bakery.Models
     static void Main()
     {
       Console.WriteLine("Welcome to Pierre's Bakery!");
+
       Console.WriteLine("Bread is $5 per loaf, with a buy 2 get 1 free promotion");
+
       Console.WriteLine("Pastries are $2 each, with a 3 for $5 promotion");
+
+      Console.WriteLine("How many loaves of bread would you like?");
+      string stringLoafOrder = Console.ReadLine();
+      int intLoafOrder = int.Parse(stringLoafOrder);
+
+      Console.WriteLine("How many pastries would you like?");
+      string stringPastryOrder = Console.ReadLine();
+      int intPastryOrder = int.Parse(stringPastryOrder);
+
+      if (intLoafOrder % 2 == 0)
+      {
+        Console.WriteLine("You scored a deal on this bread!");
+      }
+
+      if (intPastryOrder % 3 == 0)
+      {
+         Console.WriteLine("Hooray for pastry deals!");
+      }
     }
 
   }
