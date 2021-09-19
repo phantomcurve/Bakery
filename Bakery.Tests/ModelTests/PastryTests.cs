439c02222695279e73b934dca26e52b4f4d77b41
Tim Roth <timdroth@gmail.com>
2021-09-19 00:07:34 -0700

    Confirm passing pastry constructor test

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -1,12 +1,16 @@
-// using Microsoft.VisualStudio.TestTools.UnitTesting;
-// using Bakery.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
 
-// namespace Bakery.Tests
-// {
-//   [TestClass]
-//   public class PastryTests
-//   {
-//     [TestMethod]
-//     public void 
-//   }
-// }
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+    [TestMethod]
+    public void PastryConstructor_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry(2);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+  }
+}
